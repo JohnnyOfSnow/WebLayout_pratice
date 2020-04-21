59b8999475a73f7a26562c466cee836b9a9d7295
johnnyOfSnow <good258johnny@yahoo.com.tw>
2020-04-21 12:37:36 +0900

    Layout pratice 4 add article's web layout

--- layout_pratice/ex04/script.js
+++ layout_pratice/ex04/script.js
@@ -1,10 +1,22 @@
 $(function(){
 
+	//document is ready
 	$(document).ready(function(){
-		 
+		moving=0
+		$(".chapter-menu-button").click(function(){
+			if(moving==0){
+				$(".chapter-menu").animate({"left": "0px"})
+				$('.chapter-menu-button:before').addClass('chapter-menu-active');
+				moving=1
+			}else if(moving==1){
+				$(".chapter-menu").animate({"left": "-120px"})
+				moving=0
+			}else{
+
+			}
+		})
 	});
 
-	//document is ready
 	var winWidth = $(window).width(),
 		winHeight = $(window).height(),
 		curPos = $(window).scrollTop();
@@ -15,24 +27,25 @@ $(function(){
 	  });
 	});
 
-	function getScrollTop(){
-	  var bodyTop = 0;
-	  if (typeof window.pageYOffset != "undefined") {
-	    bodyTop = window.pageYOffset;
-	  } else if (typeof document.compatMode != "undefined"
-	             && document.compatMode != "BackCompat") {
-	    bodyTop = document.documentElement.scrollTop;
 
-	  } else if (typeof document.body != "undefined") {
-	    bodyTop = document.body.scrollTop;
-	  }
-	  if(bodyTop > 0){
-	  	$('.nav-icon').addClass('nav-icon-active');
-	  	$('.nav-menu').addClass('nav-menu-active');
-	  }else{
-	  	$('.nav-icon').removeClass('nav-icon-active');
-	  	$('.nav-menu').removeClass('nav-menu-active');
-	  }
+	function getScrollTop(){
+		var bodyTop = 0;
+		if (typeof window.pageYOffset != "undefined") {
+		    bodyTop = window.pageYOffset;
+		} else if (typeof document.compatMode != "undefined"
+		             && document.compatMode != "BackCompat") {
+		  bodyTop = document.documentElement.scrollTop;
+
+		} else if (typeof document.body != "undefined") {
+		  bodyTop = document.body.scrollTop;
+		}
+		if(bodyTop > 0){
+		 	$('.nav-icon').addClass('nav-icon-active');
+		  	$('.nav-menu').addClass('nav-menu-active');
+		}else{
+		  	$('.nav-icon').removeClass('nav-icon-active');
+		  	$('.nav-menu').removeClass('nav-menu-active');
+		}
 	}
 
 });
